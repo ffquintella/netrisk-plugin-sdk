28031c082c136717c96bfab11919c465510de8be
Felipe Quintella <felipe@quintella.com>
2025-05-16 16:17:28 -0300

    INC

--- Contracts/INetriskFaceIDPlugin.cs
+++ Contracts/INetriskFaceIDPlugin.cs
@@ -5,4 +5,6 @@ namespace Contracts;
 public interface INetriskFaceIDPlugin: INetriskPlugin
 {
     public SKBitmap? ExtractFace(SKBitmap bitmap);
+    
+    public float[]? ExtractEncodings(SKBitmap bitmap);
 }
