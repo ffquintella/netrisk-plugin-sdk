b2a78a22296722cc96e2714ce4dbba50d609b74b
Felipe Quintella <felipe@quintella.com>
2025-07-11 13:25:42 -0300

    Spoof Verification

--- Contracts/INetriskFaceIDPlugin.cs
+++ Contracts/INetriskFaceIDPlugin.cs
@@ -7,4 +7,6 @@ public interface INetriskFaceIDPlugin: INetriskPlugin
     public SKBitmap? ExtractFace(SKBitmap bitmap);
     
     public float[]? ExtractEncodings(SKBitmap bitmap);
+
+    public (bool, float) IsSpoofing(SKBitmap bitmap);
 }
